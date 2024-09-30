95ea6f871eca6c1705f016eb09db027e895321c6
ribhy-bishtawi <ribhy2001@gmail.com>
2024-09-30 16:43:31 +0300

    feat: add filtering functionality for selecting repository timeframe

--- lib/presentation/screens/trending_repos_screen.dart
+++ lib/presentation/screens/trending_repos_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trending_repositories/data/models/repository_model.dart';
 import 'package:trending_repositories/presentation/widgets/repo_card.dart';
+import 'package:trending_repositories/utils/constants.dart';
 import 'package:trending_repositories/utils/palette.dart';
 import 'package:trending_repositories/utils/text_style.dart';
 
@@ -25,6 +27,15 @@ class _TrendingReposScreenState extends State<TrendingReposScreen> {
     createdAt: DateTime.now(),
     htmlUrl: 'https://github.com/flutter/flutter',
   );
+
+  String _selectedTimeframe = 'day';
+
+  void _onFilterChanged(String timeframe) {
+    setState(() {
+      _selectedTimeframe = timeframe;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,11 +56,67 @@ class _TrendingReposScreenState extends State<TrendingReposScreen> {
               ))
         ],
       ),
-      body: ListView.builder(
-        itemCount: 1,
-        itemBuilder: (BuildContext context, int index) {
-          return RepoCard(repo: dummyRepo);
-        },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildFilterButton(
+                  label: 'Last Day',
+                  isSelected: _selectedTimeframe == 'day',
+                  onTap: () => _onFilterChanged('day'),
+                ),
+                _buildFilterButton(
+                  label: 'Last Week',
+                  isSelected: _selectedTimeframe == 'week',
+                  onTap: () => _onFilterChanged('week'),
+                ),
+                _buildFilterButton(
+                  label: 'Last Month',
+                  isSelected: _selectedTimeframe == 'month',
+                  onTap: () => _onFilterChanged('month'),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 1,
+              itemBuilder: (BuildContext context, int index) {
+                return RepoCard(repo: dummyRepo);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFilterButton(
+      {required String label,
+      required bool isSelected,
+      required VoidCallback onTap}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 16.w),
+        decoration: BoxDecoration(
+          color: isSelected ? Palette.primaryColor : Colors.grey[300],
+          borderRadius: Constants.borderRadius8,
+          border: Border.all(
+            color: isSelected ? Palette.primaryColor : Colors.grey,
+            width: 2,
+          ),
+        ),
+        child: Text(
+          label,
+          style: TextStyle(
+            color: isSelected ? Palette.whiteColor : Palette.header,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
